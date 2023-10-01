bee6466da01d3bd71b0a3e92a51c99ed9a5086d8
Kritik29 <Kritik29@users.noreply.github.com>
2023-10-01 02:40:07 -0400

    sympy

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sym
 
-mean_class1 = np.array([4, 7])
+x1 = sym.Symbol('x1')
+x2 = sym.Symbol('x2')
 
 X = np.array([x1, x2])
+mean_class1 = np.array([4, 7]).T
+
+a = np.matmul(mean_class1, X)
+
+print(a)
+
+
